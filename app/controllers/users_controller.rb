bd6dffe44d5eec737b6a993dac1bcbc8f01aa2dd
Jason Kroll <krolly@gmail.com>
2016-04-28 05:44:58 +0000

    Home page improvements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,12 @@ class UsersController < ApplicationController
    def show
       @user = User.find( params[:id] )
    end
-   
+
+   def edit
+      
+   end
+
    def update
       @user = User.find ( params[:id] )
-      
    end
 end
